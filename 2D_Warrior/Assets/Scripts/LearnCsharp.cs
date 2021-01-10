775a859bd8b4c27d5d02044e3b339bf28b9e999f
xyu0216 <75553488+xyu0216@users.noreply.github.com>
2021-01-10 12:00:28 +0800

    移動與跳躍

---
+++ 2D_Warrior/Assets/Scripts/LearnCsharp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LearnCsharp : MonoBehaviour
+{
+    public int a = 10, b = 3;
+
+    private void Start()
+    {
+        print(a > b);
+        print(a < b);
+        print(a == b);
+        print(a != b);
+
+        //並且&&
+        print(true && true);
+        print(true && false);
+        print(false && true);
+        print(false && false);
+
+        //或者||
+        print(true || true);
+        print(true || false);
+        print(false || true);
+        print(false || false);
+
+        //相反!
+        print(!true);
+        print(!false);
+
+            }
+
+}
--- 2D_Warrior/Assets/Scripts/Player.cs
+++ 2D_Warrior/Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
     public float MoveSpeed = 10.5f;
     [Header("跳躍高度"), Range(0, 3000)]
     public int JumpHeight = 100;
-    [Header("是否在地面上"),Tooltip("是否在地面上")]
+    [Header("是否在地面上"), Tooltip("是否在地面上")]
     public bool OnTheGround = false;
     [Header("子彈")]
     public GameObject Bullets;
@@ -16,9 +16,9 @@ public class Player : MonoBehaviour
     public int BulletSpeed = 800;
     [Header("開槍音效")]
     public AudioClip Fire;
-    [Header("血量"),Range(0,200)]
+    [Header("血量"), Range(0, 200)]
     public float HP = 100;
-    
+
     private AudioSource Aud;
     private Rigidbody2D Rig;
     private Animator Ani;
@@ -31,30 +31,65 @@ public class Player : MonoBehaviour
 
         //剛體欄位=取得原件<剛體>()
         Rig = GetComponent<Rigidbody2D>();
+        Ani = GetComponent<Animator>();
 
 
-        
 
-        Jump();
-        Jump(200);
+
+
 
         Shoot("右邊", "咻咻咻");
         Shoot("左邊", "咻咻嘿");
 
     }
+    private void Update()
+    {
+        GetHorizontal();
+        Move();
+        Jump();
 
+    }
+    /// <summary>
+    /// 移動
+    /// </summary>
     private void Move()
     {
         //剛體.加速度 = 二維 (水平*速度,0,元件原本的Y軸)
-        Rig.velocity = new Vector2(h *MoveSpeed, Rig.velocity.y);
-        
-       
+        Rig.velocity = new Vector2(h * MoveSpeed, Rig.velocity.y);
+
+        //按下 ( 按鍵 )  執行  {內容}
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //transform 指與此腳本同一層的transform
+            //Rotation在程式裡是localEulerAngle
+            transform.localEulerAngles = Vector3.zero;
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.localEulerAngles = new Vector3(0, 180, 0);
+        }
+        Ani.SetBool("跑步開關", h != 0);
+
+
     }
-    private void Jump(int height = 100)
+
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
     {
-        print("跳躍高度" + height);
+        if (OnTheGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            Rig.AddForce(new Vector2(0, JumpHeight));     //剛體.添加推力(二維向量)
+            OnTheGround = false;
+        }
+
     }
-    private void Shoot(string direction ,string sound)
+    
+       
+
+    private void Shoot(string direction, string sound)
     {
         print("開槍方向:" + direction);
         print("開槍音效:" + sound);
@@ -68,9 +103,9 @@ public class Player : MonoBehaviour
     {
     }
 
-   
-       
-        
+
+
+
     //取得水平軸向
     private void GetHorizontal()
     {
@@ -78,16 +113,12 @@ public class Player : MonoBehaviour
         h = Input.GetAxis("Horizontal");
 
     }
-    
+
     //取得玩家水平軸向的值
     public float h;
 
-    
-    private void Update()
-    {
-        GetHorizontal();
-        Move();
-    }
+
+
 
 
 }

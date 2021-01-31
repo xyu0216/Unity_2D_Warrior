5a7e3bee2b378dc0d240a9b7dd39210376f65a8f
xyu0216 <75553488+xyu0216@users.noreply.github.com>
2021-01-31 16:14:57 +0800

    更新01

---
+++ 2D_Warrior/Assets/Scripts/Bosstrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Bosstrigger : MonoBehaviour
+{
+    [Header("魔王血條")]
+    public GameObject objHP;
+    [Header("魔王")]
+    public GameObject objBoss;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "戰士")
+        {
+            objHP.SetActive(true);
+            objBoss.SetActive(true);
+        }
+
+    }
+
+
+}
